e9d0526f151fb8759cde366229c2bc914afe7d5f
dshen36 <dshen36@gatech.edu>
2015-07-27 03:16:24 -0400

    fixed dropdown box, need to figure out promises for parsing user ID

--- script.js
+++ script.js
@@ -3,10 +3,7 @@ Parse.initialize("LZns8tKGpiFr6f1NbgHatFoOAQ8KBm2m8X8SoEZF", "QGkhPTDc3qLV3mVNgi
 $(document).ready(function() {
 	$('#reportMatch').on('click',function (e) {
 		e.preventDefault();
-		// if (sessionStorage.getItem("playerRoster") !== "true") {
-		// 	console.log(sessionStorage.getItem("playerRoster"));
-			createGameForm();
-		//}
+		createGameForm();
 	})
     $('#submitData').on('click', function (e) {
 		e.preventDefault();
@@ -21,7 +18,6 @@ $(document).ready(function() {
 	    var username = $("#navBarUsername").val();
     	var password = $("#navBarPassword").val();
 		logIn(username,password);
-		// console.log(window.loggedInUser);
 	})
 	$('#logOut').on('click', function(e) {
 	    e.preventDefault();
@@ -30,7 +26,6 @@ $(document).ready(function() {
 	})
 });
 function logOut () {
-	// sessionStorage.setItem("playerRoster","false");
 	Parse.User.logOut();
 	window.location.replace("index.html");
 }
@@ -77,7 +72,6 @@ function signUp() {
 
 }
 function setCurrentUser(username) {
-	// window.loggedInUser = username;
 	sessionStorage.setItem("user",username);
 }
 function getCurrentUser() {
@@ -91,7 +85,6 @@ function logIn(username,password) {
 	  success: function(user) {
 	    for (var i = 0; i < user.length; i++) {
 	      var object = user[i];
-	      // setCurrentUser(username);
 	  	}
 	  }, 
 	  error: function(user,error) {
@@ -120,26 +113,27 @@ function logIn(username,password) {
     });
 }
 
-// Parse.User.signUp(username, password, { ACL: new Parse.ACL() }, {
-//   	success: function(user) {
-// 	    new ManageTodosView();
-// 	    self.undelegateEvents();
-// 	    delete self;
-//   	},
-//   	error: function(user, error) {
-//     	self.$(".signup-form .error").html(error.message).show();
-//     	this.$(".signup-form button").removeAttr("disabled");
-//   	}
-// });
-
-
-
 function createGameForm() {//onclick of (Report Game)
 	var Player = Parse.Object.extend("User");
 	var query = new Parse.Query(Player);
 	query.find({
 		success: function(players) {
 			console.log(players.length);
+			var selectW = document.getElementById("winnerName");
+			var lengthW = selectW.options.length;
+			if (lengthW >= 2) { //this gets rid of the text "select the winner" when you reclick on the button
+				for (i = 0; i < lengthW; i++) {
+				  selectW.options[0] = null;
+				}
+			}
+			var selectL = document.getElementById("loserName");
+			var lengthL = selectL.options.length;
+			if (lengthL >= 2) { //this gets rid of the text "select the winner" when you reclick on the button
+				for (i = 0; i < lengthL; i++) {
+				  selectL.options[0] = null;
+				}
+			}
+			//<option value="" disabled selected>Select the Winner</option>
 			for (var i = 0; i < players.length; i++) { 
   				var player = players[i];
   				if(sessionStorage.getItem(String(player.get('name'))) !== String(player.get('name'))) {
@@ -171,25 +165,154 @@ function submitGame() {
 	var loser = $("#loserName").val();
 	var winner_Score = parseInt($("#scoreW").val());
 	var loser_Score = parseInt($("#scoreL").val());
-	
-	//setting the statistics of the game
+	var winnerID = "";
+	var loserID = "";
+
 	match.set("loser",loser);
 	match.set("winner",winner);
 	match.set("winnerScore",winner_Score);
 	match.set("loserScore",loser_Score);
+	//getting the userfrom the database
+	winnerID = findUserAttr(winner,'name','objectId').then(function() {
+		loserID = findUserAttr(loser,'name','objectId');
+		return findUserAttr(loser,'name','objectId');
+	}).then(function() {
+		return match.save(null);
+	}).then(function(match) {
+		alert('new match recorded with an id of: ' +match.id);
+	}, function(error) {
+		console.log(error.message);
+		alert('there was an issue with this record');
+	});
 
-	//actually saving the game or throwing an error
-	match.save(null, {
-		success: function(match) {
-			console.log(winner);
-			alert('new match recorded with an id of: ' +match.id);
-			console.log("game recorded!");
-			//closeModal();
+
+}
+
+function findUserAttr(given,givenAttr,desiredAttr) {
+	var Player = Parse.Object.extend("User");
+	var query = new Parse.Query(Player);//Player
+	query.equalTo(givenAttr,given);
+	query.find({
+		success: function(players) {
+			console.log(players.length);
+			for (var i = 0; i < players.length; i++) { 
+  				var player = players[i];//assumably only 1
+  				//console.log("player: " + player.get('school'));
+  				if (desiredAttr === 'objectId') {
+						var desired = player.id;
+  					console.log(desired);
+  				} else {
+  					var desired = String(player.get(desiredAttr));
+  				}
+  				//console.log("The desired value is: " + desired);
+  			}
+			return desired;
 		},
-		error: function(match,error) {
-			
-			console.log(error.message);
-			alert('there was an issue with this record');
+		error: function (error) {
+			alert("Error: " + error.code + " " + error.message);
 		}
 	});
 }
+
+// function desiredCredentials(desired,desiredAttr) { //for search bar
+// 	var Player = Parse.Object.extend("User");
+// 	var query = new Parse.Query(Player);
+// 	query.equalTo(desiredAttr,desired);
+// 	query.find({
+// 		success: function(user) {
+
+// 		},
+// 		error: function (error) {
+// 			alert("Error: " + error.code + " " + error.message);
+// 		}
+// 	});
+// }
+
+
+// Parse.User.signUp(username, password, { ACL: new Parse.ACL() }, {
+//   	success: function(user) {
+// 	    new ManageTodosView();
+// 	    self.undelegateEvents();
+// 	    delete self;
+//   	},
+//   	error: function(user, error) {
+//     	self.$(".signup-form .error").html(error.message).show();
+//     	this.$(".signup-form button").removeAttr("disabled");
+//   	}
+// });
+
+
+// function findUserAttr(given,givenAttr,desiredAttr) {
+// 	var Player = Parse.Object.extend("User");
+// 	var query = new Parse.Query(Player);//Player
+// 	query.equalTo(givenAttr,given);
+// 	query.find({
+// 		success: function(players) {
+// 			console.log(players.length);
+// 			for (var i = 0; i < players.length; i++) { 
+//   				var player = players[i];//assumably only 1
+//   				//console.log("player: " + player.get('school'));
+//   				if (desiredAttr === 'objectId') {
+//						var desired = player.id;
+//   					console.log(desired);
+//   					return desired;
+//   				} else {
+//   					var desired = String(player.get(desiredAttr));
+//   					return desired;
+//   				}
+//   				//console.log("The desired value is: " + desired);
+//   			}
+// 		},
+// 		error: function (error) {
+// 			alert("Error: " + error.code + " " + error.message);
+// 		}
+// 	});
+// }
+
+// function submitGame() {
+// 	//creating a new instance of the game
+// 	var Game = Parse.Object.extend("Game");
+// 	var match = new Game();
+
+// 	//retrieving the values of from the form
+// 	var winner = $("#winnerName").val();
+// 	var loser = $("#loserName").val();
+// 	var winner_Score = parseInt($("#scoreW").val());
+// 	var loser_Score = parseInt($("#scoreL").val());
+// 	var winnerID = "";
+// 	var loserID = "";
+
+// 	// match.set("loser",loser);
+// 	// match.set("winner",winner);
+// 	// match.set("winnerScore",winner_Score);
+// 	// match.set("loserScore",loser_Score);
+// 	//getting the userfrom the database
+// 	winnerID = findUserAttr(winner,'name','objectId').then(function() {
+// 		loserID = findUserAttr(loser,'name','objectId').then(function() {
+// 			//console.log(winnerID);
+
+// 			//setting the statistics of the game
+// 			// match.set("loser",loser);
+// 			// match.set("winner",winner);
+// 			// match.set("winnerScore",winner_Score);
+// 			// match.set("loserScore",loser_Score);
+
+// 			//actually saving the game or throwing an error
+// 			match.save(null, {
+// 				success: function(match) {
+// 					console.log(winner);
+// 					alert('new match recorded with an id of: ' +match.id);
+// 					console.log("game recorded!");
+// 					//closeModal();
+// 				},
+// 				error: function(match,error) {
+					
+// 					console.log(error.message);
+// 					alert('there was an issue with this record');
+// 				}
+// 			});
+// 		});
+// 		return loserID;
+// 	});
+// 	return winnerID;
+// }

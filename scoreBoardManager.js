8c26da507f430bb0d64e3558eeaf2e08a50d4029
dshen36 <dshen200@COTNPL-9S7N3Q1.cable.comcast.com>
2015-08-04 16:07:21 -0600

    made the scoreboard dynamic so that it readjusts to screen size and mantains it's form
    ^

--- scoreBoardManager.js
+++ scoreBoardManager.js
@@ -5,4 +5,12 @@ $(document).ready(function() {
 	var test2 = document.getElementById("playerB");
 	var width = (test2.clientWidth + 1) + "px";
 	console.log(" widthB: " + width);
+
+	var scoreBoardHeight = document.getElementById("scoreBoardAnchor");
+	var pScoreA = document.getElementById("playerScoreA");
+	var heightSB = (scoreBoardHeight.clientHeight);
+	var heightPSA = (pScoreA.clientHeight);
+	console.log("heightSB: " + heightSB + " ,heightPSA:" + heightPSA);
+	var difference = heightPSA - heightSB; //not the actual height where it is anchored, but the height from top to bottom
+	console.log("difference in pixels: " + difference);
 });

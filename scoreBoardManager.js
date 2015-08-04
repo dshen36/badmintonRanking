2ec60426514f1d6e169c2ccc849bf382bc7ec453
dshen36 <dshen200@COTNPL-9S7N3Q1.cable.comcast.com>
2015-08-04 14:17:49 -0600

    some edits

---
+++ scoreBoardManager.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	var test = document.getElementById("playerA");
+	var width = (test.clientWidth + 1) + "px";
+	console.log(" widthA: " + width);
+	var test2 = document.getElementById("playerB");
+	var width = (test2.clientWidth + 1) + "px";
+	console.log(" widthB: " + width);
+});

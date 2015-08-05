2474db602cbd2c1f45a36c2a7bea362b4f562190
dshen36 <dshen200@COTNPL-9S7N3Q1.cable.comcast.com>
2015-08-05 16:30:42 -0600

    score increment/undo works
    need to figure out a timer
    need to place the buttons correctly

--- scoreBoardManager.js
+++ scoreBoardManager.js
@@ -1,16 +1,66 @@
 $(document).ready(function() {
-	var test = document.getElementById("playerA");
-	var width = (test.clientWidth + 1) + "px";
-	console.log(" widthA: " + width);
-	var test2 = document.getElementById("playerB");
-	var width = (test2.clientWidth + 1) + "px";
-	console.log(" widthB: " + width);
+	// var test = document.getElementById("playerA");
+	// var width = (test.clientWidth + 1) + "px";
+	// console.log(" widthA: " + width);
+	// var test2 = document.getElementById("playerB");
+	// var width = (test2.clientWidth + 1) + "px";
+	// console.log(" widthB: " + width);
 
-	var scoreBoardHeight = document.getElementById("scoreBoardAnchor");
-	var pScoreA = document.getElementById("playerScoreA");
-	var heightSB = (scoreBoardHeight.clientHeight);
-	var heightPSA = (pScoreA.clientHeight);
-	console.log("heightSB: " + heightSB + " ,heightPSA:" + heightPSA);
-	var difference = heightPSA - heightSB; //not the actual height where it is anchored, but the height from top to bottom
-	console.log("difference in pixels: " + difference);
+	// var scoreBoardHeight = document.getElementById("scoreBoardAnchor");
+	// var pScoreA = document.getElementById("playerScoreA");
+	// var heightSB = (scoreBoardHeight.clientHeight);
+	// var heightPSA = (pScoreA.clientHeight);
+	// console.log("heightSB: " + heightSB + " ,heightPSA:" + heightPSA);
+	// var difference = heightPSA - heightSB; //not the actual height where it is anchored, but the height from top to bottom
+	// console.log("difference in pixels: " + difference);
+	$('#pointA').on('click',function (e) {
+		e.preventDefault();
+		pointManager('#pointA');
+	})
+	$('#pointB').on('click',function (e) {
+		e.preventDefault();
+		pointManager('#pointB');
+	})
+	$('#undoPoint').on('click',function (e) {
+		e.preventDefault();
+		undoPoint();
+	})
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+	var scoreA = 0;
+	var scoreB = 0;
+	// var lastRallyWinner = "";
+	var numRallies = 1; //this is going to keep track of who won which points. Also useful for undoPoint
+	var rallyTracker = [];
+	function pointManager(winner) {
+		if (winner === '#pointA') {
+			scoreA += 1;
+			document.getElementById("playerScoreA").innerHTML = scoreA;
+		} else if (winner === '#pointB') {
+			scoreB += 1;
+			document.getElementById("playerScoreB").innerHTML = scoreB;
+		}
+		// lastRallyWinner = winner;
+		rallyTracker[numRallies-1] = winner;
+		numRallies+=1;
+		console.log(numRallies);
+	}
+	function undoPoint() {
+		numRallies -= 1;
+		if (numRallies >= 1) {
+			var lastWinner = rallyTracker[numRallies-1];
+		} else {
+			return;
+		}
+
+		if (lastWinner === '#pointA') {
+			scoreA -= 1;
+			document.getElementById("playerScoreA").innerHTML = scoreA;
+		} else if (lastWinner === '#pointB') {
+			scoreB -= 1;
+			document.getElementById("playerScoreB").innerHTML = scoreB;
+		}
+		console.log(numRallies);
+	}
 });
